898c44137876de41d16095a01316676d6254ec18
codeKGB <kbustuv@gmail.com>
2025-12-10 08:30:17 +0545

    Worked on a traffic generator used a .data file to stoer its inforamtion

--- generator.py
+++ generator.py
@@ -2,19 +2,29 @@ import random
 from queue import Vehicle
 import time
 
-
+data_file = "Traffic.data"
 LANES = ["A","B","C","D"]
 
+def record (Vehicle) :
+    with open(data_file, "a") as file:
+        file.write(f"{Vehicle.lane},{Vehicle.id},{Vehicle.time}\n")
 
-def generate_vehicle (mean_interval = 2.0):
+def generate_vehicle (delay = 2.0):
     while True :
         road = random.choice(LANES)
         vehicle_id = random.randint(1,10000)
-        interval = random.expovariate(1.0 / mean_interval)
+        interval = random.expovariate(1.0 / delay)
         time.sleep(interval)
+        return Vehicle(vehicle_id, road,interval)
 
-def record_vehicles() :
-    pass
+def generator():
+    while True:
+        v=generate_vehicle()
+        record(v)
+
+
+generator()
+
 
 
 
