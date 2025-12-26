486fb2a9c510874ba290b20206d18bf0e8c00abf
codeKGB <kbustuv@gmail.com>
2025-12-26 21:15:03 +0545

    Initialized pygame for 2 lane junction simulator

--- src/Simulator.py
+++ src/Simulator.py
@@ -1,10 +1,4 @@
 import pygame
-import threading
-import sys
-import time
-from Lanes import LaneManager
-from Traffic_controller import TrafficController
-from Generator import generator
 
 WINDOW_WIDTH = 900
 WINDOW_HEIGHT = 900
@@ -19,3 +13,10 @@ COLOR_WHITE = (255, 255, 255)
 COLOR_RED = (255, 50, 50)
 COLOR_GREEN = (50, 255, 50)
 COLOR_ORANGE = (255, 165, 0)
+
+pygame.init()
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Traffic Simulation (Fixed Timings)")
+font = pygame.font.SysFont('Arial', 16, bold=True)
+
+
