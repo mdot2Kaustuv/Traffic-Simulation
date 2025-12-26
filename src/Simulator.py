1664407ad38c39835eced08e5e7c490e0cfea46f
codeKGB <kbustuv@gmail.com>
2025-12-26 21:22:00 +0545

    Updated pygame.py , now it has roads and lights

--- src/Simulator.py
+++ src/Simulator.py
@@ -79,12 +79,71 @@ class VisualVehicle:
             return self.x > self.stop_boundary
         return False
 
+    def update(self, light_is_green, vehicle_ahead):
+        if not self.crossed_stop_line:
+                self.crossed_stop_line = self.check_stop_line()
 
+            should_move = True
 
+            if not self.crossed_stop_line and not light_is_green:
+                dist_to_line = abs((self.y if self.road in "AB" else self.x) - self.stop_boundary)
+                if dist_to_line < 10:
+                    should_move = False
 
+            if vehicle_ahead:
+                distance = self.get_distance_to(vehicle_ahead)
+                if distance < 50:
+                    should_move = False
+            if should_move:
+                self.x += self.dir[0] * self.speed
+                self.y += self.dir[1] * self.speed
 
+    def get_distance_to(self, other_vehicle):
+            if self.road == "A":
+                return other_vehicle.y - self.y
+            elif self.road == "B":
+                return self.y - other_vehicle.y
+            elif self.road == "C":
+                return self.x - other_vehicle.x
+            elif self.road == "D":
+                return other_vehicle.x - self.x
+            return float('inf')
 
+    def draw(self, surface):
+            surface.blit(self.image, (self.x - self.image.get_width() // 2, self.y - self.image.get_height() // 2))
 
+    def draw_env(surface, controller):
+        surface.fill(COLOR_BG)
+        pygame.draw.rect(surface, COLOR_ROAD, (CENTER_X - ROAD_WIDTH, 0, ROAD_WIDTH * 2, WINDOW_HEIGHT))
+        pygame.draw.rect(surface, COLOR_ROAD, (0, CENTER_Y - ROAD_WIDTH, WINDOW_WIDTH, ROAD_WIDTH * 2))
+
+        # Stop Lines
+        pygame.draw.line(surface, COLOR_WHITE, (CENTER_X - ROAD_WIDTH, CENTER_Y - ROAD_WIDTH - 5),
+                         (CENTER_X, CENTER_Y - ROAD_WIDTH - 5), 4)  # A
+        pygame.draw.line(surface, COLOR_WHITE, (CENTER_X, CENTER_Y + ROAD_WIDTH + 5),
+                         (CENTER_X + ROAD_WIDTH, CENTER_Y + ROAD_WIDTH + 5), 4)  # B
+        pygame.draw.line(surface, COLOR_WHITE, (CENTER_X + ROAD_WIDTH + 5, CENTER_Y - ROAD_WIDTH),
+                         (CENTER_X + ROAD_WIDTH + 5, CENTER_Y), 4)  # C
+        pygame.draw.line(surface, COLOR_WHITE, (CENTER_X - ROAD_WIDTH - 5, CENTER_Y),
+                         (CENTER_X - ROAD_WIDTH - 5, CENTER_Y + ROAD_WIDTH), 4)  # D
+
+        # Lines
+        pygame.draw.line(surface, COLOR_YELLOW, (CENTER_X, 0), (CENTER_X, WINDOW_HEIGHT), 3)
+        pygame.draw.line(surface, COLOR_YELLOW, (0, CENTER_Y), (WINDOW_WIDTH, CENTER_Y), 3)
+
+        # Traffic Lights
+        light_coords = {
+            "A": (CENTER_X - ROAD_WIDTH - 30, CENTER_Y - ROAD_WIDTH - 30),
+            "B": (CENTER_X + ROAD_WIDTH + 30, CENTER_Y + ROAD_WIDTH + 30),
+            "C": (CENTER_X + ROAD_WIDTH + 30, CENTER_Y - ROAD_WIDTH - 30),
+            "D": (CENTER_X - ROAD_WIDTH - 30, CENTER_Y + ROAD_WIDTH + 30)
+        }
+        for road, pos in light_coords.items():
+            col = COLOR_GREEN if controller.lights[road].is_green() else COLOR_RED
+            pygame.draw.circle(surface, (30, 30, 30), pos, 20)
+            pygame.draw.circle(surface, col, pos, 15)
+            label = font.render(road, True, COLOR_WHITE)
+            surface.blit(label, (pos[0] - 5, pos[1] - 35))
 
 
 if __name__ == "__main__":
